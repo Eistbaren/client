b5b0a5c708620ddbec1b6ad13a8bc731ea558af2
Dorian Zedler <dorian@itsblue.de>
2022-05-17 12:24:31 +0200

    Fix: Type for .svg

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Bootstrap function
+ * @return {JSX.Element}
+ */
 function App() {
   return (
     <div className="App">
