09b5ba2016f21abc67cf10a0344549719ef66c11
Dorian Zedler <dorian@itsblue.de>
2022-05-17 14:28:30 +0200

    Style: Use prettier to format all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,39 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'google',
     'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
     'plugin:@typescript-eslint/eslint-recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:css/recommended',
+    'plugin:prettier/recommended',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-  ],
-  'rules': {
+  plugins: ['react', '@typescript-eslint', 'prettier', 'css'],
+  rules: {
     'react/react-in-jsx-scope': 'off',
+    indent: ['error', 2],
   },
-  'settings': {
-    'react': {
-      'version': 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
     },
     'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.jsx', '.ts', '.tsx'],
-        'paths': ['./src'],
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+        paths: ['./src'],
       },
     },
   },
