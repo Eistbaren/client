1bbcb57afb38f11c6d97a1005c407c4a1e68da5e
Dorian Zedler <dorian@itsblue.de>
2022-05-24 13:02:22 +0200

    Feat: Add mui and sketch landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  AppBar,
+  Toolbar,
+  Stepper,
+  StepLabel,
+  Step,
+  Box,
+  Typography,
+  Container,
+  CssBaseline,
+} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Landingpage from './Landingpage';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 /**
  * Bootstrap function
  * @return {JSX.Element}
  */
 function App() {
+  const steps = [
+    'Find a place to eat',
+    'Select a timeslot and table',
+    'Reserve your table',
+  ];
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+    <React.Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+              Reservation bear
+            </Typography>
+            <Box sx={{ width: '80%', flexGrow: 1 }}>
+              <Stepper activeStep={0}>
+                {steps.map(label => (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                ))}
+              </Stepper>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Container
+          component='main'
+          maxWidth='lg'
+          sx={{ my: { xs: 8, md: 12 } }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Landingpage></Landingpage>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
