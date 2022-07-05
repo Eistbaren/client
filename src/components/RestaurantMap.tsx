6f3c6ffd51d871fe2005e8ee56094a478797dfdb
NotSomeBot <43829743+mariusdkm@users.noreply.github.com>
2022-07-05 14:47:49 +0200

    🐻‍❄️ Feat: Clickable locations and close popups (#72)
    * Feat: make cursor change to pointer
    
    * Feat: Dorians wishes
    
    * Change icon colors

--- src/components/RestaurantMap.tsx
+++ src/components/RestaurantMap.tsx
@@ -3,7 +3,14 @@ import { fromLonLat, toLonLat } from 'ol/proj';
 import { Point, Circle } from 'ol/geom';
 import 'ol/ol.css';
 
-import { RMap, ROSM, RLayerVector, RFeature, RPopup } from 'rlayers';
+import {
+  RMap,
+  ROSM,
+  RLayerVector,
+  RFeature,
+  ROverlay,
+  RFeatureUIEvent,
+} from 'rlayers';
 import { RStyle, RIcon, RStroke } from 'rlayers/style';
 
 import '../css/RestaurantMap.css';
@@ -11,6 +18,9 @@ import RestaurantCard from '../components/RestaurantCard';
 
 import { GeographicCoordinates } from '../data';
 import { useTheme } from '@mui/material';
+import { Geometry } from 'ol/geom';
+import Feature from 'ol/Feature';
+import React, { useCallback } from 'react';
 
 /**
  * OnClick callback
@@ -34,6 +44,10 @@ export default function RestaurantMap(params: {
 }) {
   const { restaurants, onClick, isLoading, center, setCenter, range } = params;
 
+  const [currentRestaurant, setCurrentRestaurant] = React.useState(
+    null as Feature<Geometry> | null,
+  );
+
   const centerCords = fromLonLat([
     center.lon ?? 11.574231,
     center.lat ?? 48.139244,
@@ -47,6 +61,9 @@ export default function RestaurantMap(params: {
       }
       initial={{ center: centerCords, zoom: 12 }}
       onClick={e => {
+        if (!e.map.hasFeatureAtPixel(e.map.getEventPixel(e.originalEvent))) {
+          setCurrentRestaurant(null);
+        }
         const c: Array<number> = toLonLat(
           e.map.getCoordinateFromPixel(e.pixel),
         );
@@ -54,7 +71,15 @@ export default function RestaurantMap(params: {
       }}
     >
       <ROSM />
-      <RLayerVector zIndex={10}>
+      <RLayerVector
+        zIndex={10}
+        onPointerEnter={useCallback((e: RFeatureUIEvent) => {
+          e.map.getTargetElement().style.cursor = 'pointer';
+        }, [])}
+        onPointerLeave={useCallback((e: RFeatureUIEvent) => {
+          e.map.getTargetElement().style.cursor = 'initial';
+        }, [])}
+      >
         {restaurants.map(restaurant =>
           restaurant.location &&
           restaurant.location.lat &&
@@ -69,27 +94,47 @@ export default function RestaurantMap(params: {
                 )
               }
               key={restaurant.id}
+              onClick={e => {
+                if (e.target !== currentRestaurant) {
+                  e.target.set('restaurant', restaurant);
+                  setCurrentRestaurant(e.target);
+                } else {
+                  setCurrentRestaurant(null);
+                }
+              }}
             >
               <RStyle>
-                <RIcon src='/images/location.svg' anchor={[0.5, 0.8]} />
+                <RIcon
+                  src='/images/location.svg'
+                  anchor={[0.5, 0.8]}
+                  scale={0.2}
+                />
               </RStyle>
-              <RPopup trigger={'click'} className='restaurant-overlay'>
-                <RestaurantCard
-                  restaurant={restaurant}
-                  onClick={() => onClick(restaurant)}
-                  dontShowImages={true}
-                ></RestaurantCard>
-              </RPopup>
             </RFeature>
           ) : null,
         )}
+      </RLayerVector>
+      <RLayerVector zIndex={10}>
+        {currentRestaurant ? (
+          <div>
+            <RFeature geometry={currentRestaurant.getGeometry()}>
+              <ROverlay className='restaurant-overlay' autoPosition={true}>
+                <RestaurantCard
+                  restaurant={currentRestaurant.get('restaurant')}
+                  onClick={() => onClick(currentRestaurant.get('restaurant'))}
+                  dontShowImages={true}
+                ></RestaurantCard>
+              </ROverlay>
+            </RFeature>
+          </div>
+        ) : null}
       </RLayerVector>
       <RLayerVector zIndex={9}>
         <RFeature geometry={new Circle(centerCords, range * 1000)}>
           <RStyle>
             <RStroke
               color={
-                center.lat ? useTheme().palette.primary.main : 'transparent'
+                center.lat ? useTheme().palette.primary.dark : 'transparent'
               }
               width={4}
             />
