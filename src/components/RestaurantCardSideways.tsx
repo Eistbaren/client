56c7f05f0e70a11dbaa6ebefc52e15e58cb38ab7
NotSomeBot <43829743+mariusdkm@users.noreply.github.com>
2022-07-15 19:13:45 +0200

    🚀 Feat: details page redesign (#99)
    * Merge: Add Restaurant on Details page
    
    * fix: css style
    
    * Update src/pages/ReservationDetails.tsx
    
    * Fix: css styling
    
    * Fix: don't show reserve button
    
    * Fix: include requested changes
    
    * Feat: redesign reservationdetails
    
    * Fix: Redo style
    
    * rebase: delete file
    
    * Fix: Links os restaurants not working (#100)
    
    * Fix: don't show reserve button
    
    * Fix: include requested changes
    
    * Merge: Add Restaurant on Details page
    
    * Fix: don't show reserve button
    
    * Fix: merge stuff
    
    * Fix: wrong link
    
    * fix: delete ReservationDetails.css
    
    * Fix: Include requested changes
    
    * Fix: Don't show restaurant when not showing reservation
    
    * Fix: Data alignment
    
    * Fix: center stuff
    
    * Fix: Change shadows
    
    Co-authored-by: Dorian Zedler <dorian@itsblue.de>

---
+++ src/components/RestaurantCardSideways.tsx
@@ -0,0 +1,91 @@
+import {
+  Card,
+  CardMedia,
+  Typography,
+  CardContent,
+  Link,
+  Skeleton,
+} from '@mui/material';
+
+import { Restaurant } from '../data/api';
+import React from 'react';
+import { Context } from '../data/Context';
+
+/**
+ * OnClick callback
+ * @name OnClickCallback
+ * @function
+ */
+
+/**
+ * RestaurantCard
+ * @param  {{restaurant: Restaurant, onClick: OnClickCallback}} params accepts restaurant object and onclick callback
+ * @return {JSX.Element}
+ */
+export default function RestaurantCardSideways(params: {
+  restaurant?: Restaurant;
+  numberOfSeats?: number;
+  onClick: () => void;
+}) {
+  const { restaurant, onClick, numberOfSeats } = params;
+  const { configuration } = React.useContext(Context);
+
+  const image: string =
+    restaurant?.images?.at(0) ?? '069f72db-2157-43de-8e88-21661b518100';
+
+  return (
+    <>
+      <Card
+        onClick={onClick}
+        className='restaurant-card'
+        sx={{ display: 'flex', boxShadow: 5, backgroundColor: 'grey.A200' }}
+      >
+        {restaurant ? (
+          <CardMedia
+            component='img'
+            sx={{ width: 151 }}
+            image={`${configuration.basePath}/image/${image}`}
+          />
+        ) : (
+          <Skeleton variant='rectangular' width={151} height={127} />
+        )}
+        <CardContent style={{ width: '100%' }}>
+          <Typography
+            gutterBottom
+            variant='h6'
+            component='div'
+            className='restaurant-card-title'
+            width='100%'
+          >
+            {restaurant?.name ?? <Skeleton variant='text' width='100%' />}
+          </Typography>
+          <Typography gutterBottom variant='body2' component='div' width='100%'>
+            {numberOfSeats ? (
+              `${numberOfSeats} reserved seats`
+            ) : (
+              <Skeleton variant='text' />
+            )}
+          </Typography>
+          {restaurant ? (
+            <Link
+              width='100%'
+              href={
+                restaurant.website?.includes('://')
+                  ? restaurant.website
+                  : `//${restaurant.website}`
+              }
+              target='_blank'
+              onClick={e => {
+                e.stopPropagation();
+              }}
+            >
+              Visit website
+            </Link>
+          ) : (
+            <Skeleton variant='text' />
+          )}
+        </CardContent>
+      </Card>
+    </>
+  );
+}
--- src/pages/ReservationDetails.tsx
+++ src/pages/ReservationDetails.tsx
@@ -2,13 +2,16 @@ import {
   Alert,
   AlertColor,
   AlertTitle,
+  Box,
   Button,
   Chip,
   Collapse,
+  Grid,
   Skeleton,
   Stack,
   TextField,
   Tooltip,
+  Typography,
 } from '@mui/material';
 import { DatePicker, TimePicker } from '@mui/x-date-pickers';
 import CloseIcon from '@mui/icons-material/Close';
@@ -17,10 +20,10 @@ import LoadingButton from '@mui/lab/LoadingButton';
 import { useContext, useEffect, useState } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 
-import '../css/ReservationDetails.css';
 import { Reservation, Restaurant } from '../data';
 import { Context } from '../data/Context';
 import RestaurantDetailsModal from '../components/RestaurantDetailsModal';
+import RestaurantCardSideways from '../components/RestaurantCardSideways';
 
 /**
  * Reservation Approval Page
@@ -158,215 +161,239 @@ export default function ReservationApproval() {
 
   return (
     <>
-      <div className='booking-summary-container'>
-        <div className='booking-summary-header'>
-          <h3>Reservation summary</h3>
-          {reservation ? (
-            <Tooltip title='You will recieve an email with a confirmation link 24 hours before your reservation'>
-              {reservation.confirmed ? (
-                <Chip icon={<CheckIcon />} label='Confirmed' color='success' />
-              ) : (
-                <Chip
-                  icon={<CloseIcon />}
-                  label='Not Confirmed'
-                  color='error'
-                />
-              )}
-            </Tooltip>
-          ) : null}
-        </div>
-        <Collapse in={showAlert}>
-          <Alert
-            severity={alert?.severity}
-            style={{ marginBottom: '10px', marginTop: '10px' }}
-            variant='outlined'
-            onClose={showReservation ? () => setShowAlert(false) : undefined}
-          >
-            <AlertTitle>{alert?.title}</AlertTitle>
-            {alert?.body}
-          </Alert>
-        </Collapse>
-        {showReservation ? (
-          <>
-            <div className='booking-summary-restaurant-container'>
-              <div>
-                <p className='booking-summary-label'>Restaurant</p>
-                {restaurant ? (
-                  <TextField
-                    InputProps={{
-                      readOnly: true,
-                      onClick: () => {
-                        console.log('a');
-                        setDetailModalOpen(true);
-                      },
-                    }}
-                    className='booking-summary-restaurant'
-                    id='restaurant-name'
-                    type='text'
-                    defaultValue={restaurant.name}
-                    size='small'
-                  />
-                ) : (
-                  <Skeleton variant='rectangular' height={25} />
-                )}
-              </div>
-              <div>
-                <p className='booking-summary-label'>max # of seats</p>
-                {reservation ? (
-                  <TextField
-                    id='outlined-number'
-                    type='text'
-                    value={numberOfSeats}
-                    size='small'
-                    disabled
-                  />
-                ) : (
-                  <Skeleton variant='rectangular' height={25} />
-                )}
-              </div>
-            </div>
-            {reservation ? (
-              <div className='booking-summary-date-time-container'>
-                <div style={{ width: '40%' }}>
-                  <p className='booking-summary-label'>From</p>
-                  <TimePicker
-                    onChange={() => {
-                      return;
-                    }}
-                    readOnly
-                    value={(reservation?.time?.from ?? 0) * 1000}
-                    renderInput={params => (
-                      <TextField {...params} size='small' disabled />
-                    )}
-                    ampm={false}
-                  />
-                </div>
-                <div style={{ width: '40%' }}>
-                  <p className='booking-summary-label'>To</p>
-                  <TimePicker
-                    value={(reservation?.time?.to ?? 0) * 1000}
-                    readOnly
-                    onChange={() => {
-                      return;
-                    }}
-                    renderInput={params => (
-                      <TextField {...params} size='small' disabled />
-                    )}
-                    ampm={false}
-                  />
-                </div>
-                <div>
-                  <p className='booking-summary-label'>Date</p>
-                  <DatePicker
-                    value={(reservation?.time?.from ?? 0) * 1000}
-                    onChange={() => {
-                      return;
-                    }}
-                    renderInput={params => (
-                      <TextField {...params} size='small' fullWidth disabled />
-                    )}
-                    disabled={true}
-                  />
-                </div>
-              </div>
-            ) : (
-              <div className='booking-summary-date-time-container'>
-                <div style={{ width: '30%' }}>
-                  <p className='booking-summary-label'>From</p>
-                  <Skeleton variant='rectangular' height={25} />
-                </div>
-                <div style={{ width: '30%' }}>
-                  <p className='booking-summary-label'>To</p>
-                  <Skeleton variant='rectangular' height={25} />
-                </div>
-                <div style={{ width: '40%' }}>
-                  <p className='booking-summary-label'>Date</p>
-                  <Skeleton variant='rectangular' height={25} />
-                </div>
-              </div>
-            )}
-
-            <p className='booking-summary-label'>Name</p>
-            {reservation ? (
-              <TextField
-                id='nameInput'
-                defaultValue={reservation?.userName}
-                type='text'
-                InputProps={{
-                  readOnly: true,
-                }}
-                size='small'
-                disabled
-              />
-            ) : (
-              <Skeleton variant='rectangular' height={25} />
-            )}
-
-            <p className='booking-summary-label'>Email</p>
-            {reservation ? (
-              <TextField
-                id='emailInput'
-                defaultValue={reservation?.userEmail}
-                type='email'
-                InputProps={{
-                  readOnly: true,
-                }}
-                size='small'
-                disabled
-              />
-            ) : (
-              <Skeleton variant='rectangular' height={25} />
-            )}
-
-            <Stack
-              direction='column'
-              spacing={2}
-              className='booking-summary-cta-container'
-            >
-              {reservation && !reservation.confirmed && confirmationToken && (
-                <LoadingButton
-                  variant='contained'
-                  color='success'
-                  size='large'
-                  sx={{ boxShadow: 3, color: 'white' }}
-                  onClick={() => confirmReservation()}
-                  loading={isConfirmButtonLoading}
-                  disabled={isCancelButtonLoading}
-                >
-                  Confirm Reservation
-                </LoadingButton>
-              )}
-              {reservation && !isIn12Hours(reservation.time?.from ?? 0) && (
-                <LoadingButton
-                  variant='contained'
-                  color='error'
-                  size='large'
-                  sx={{ boxShadow: 3, color: 'white' }}
-                  onClick={() => cancelReservation()}
-                  loading={isCancelButtonLoading}
-                  disabled={isConfirmButtonLoading}
-                >
-                  Cancel Reservation
-                </LoadingButton>
-              )}
-              {reservation && (
-                <Button
-                  variant='outlined'
-                  color='secondary'
-                  size='large'
-                  href={`${configuration.basePath}/reservation/${reservation.id}/ics`}
-                  target='_blank'
-                  rel='noreferrer'
-                  sx={{ boxShadow: 3 }}
-                  download
-                >
-                  Download ICS File
-                </Button>
+      <Grid
+        container
+        spacing={0}
+        gap={8}
+        sx={{
+          backgroundColor: '#5e81ac5b',
+          padding: '60px',
+          alignSelf: 'center',
+          margin: 'auto 0',
+        }}
+      >
+        <Grid container item xs={5}>
+          <Stack spacing={3}>
+            <Stack direction='row' spacing={2} alignItems='center'>
+              <Typography variant='h4' sx={{ color: 'primary.contrastText' }}>
+                Your Reservation
+              </Typography>
+              {showReservation && reservation && (
+                <Tooltip title='You will recieve an email with a confirmation link 24 hours before your reservation'>
+                  {reservation.confirmed ? (
+                    <Chip
+                      icon={<CheckIcon />}
+                      label='Confirmed'
+                      color='success'
+                    />
+                  ) : (
+                    <Chip
+                      icon={<CloseIcon />}
+                      label='Not Confirmed'
+                      color='error'
+                    />
+                  )}
+                </Tooltip>
               )}
             </Stack>
-          </>
-        ) : null}
-      </div>
+            <Box
+              component='img'
+              sx={{
+                width: '80%',
+              }}
+              alt='Eistbären logo'
+              src='/logo-big.png'
+            />
+          </Stack>
+        </Grid>
+        <Grid
+          container
+          item
+          marginTop={-2}
+          xs={6}
+          spacing={2}
+          alignContent='center'
+          gap={2}
+        >
+          <Grid item xs={12}>
+            <Collapse in={showAlert}>
+              <Alert
+                severity={alert?.severity}
+                style={{ marginBottom: '10px', marginTop: '10px' }}
+                variant='outlined'
+                onClose={
+                  showReservation ? () => setShowAlert(false) : undefined
+                }
+              >
+                <AlertTitle>{alert?.title}</AlertTitle>
+                {alert?.body}
+              </Alert>
+            </Collapse>
+          </Grid>
+          {showReservation && (
+            <Grid item xs={12}>
+              <RestaurantCardSideways
+                restaurant={restaurant}
+                numberOfSeats={numberOfSeats}
+                onClick={() => setDetailModalOpen(true)}
+              />
+            </Grid>
+          )}
+          {showReservation &&
+            (reservation ? (
+              <>
+                <Grid container item spacing={2}>
+                  <Grid item xs={3}>
+                    <TimePicker
+                      label='From'
+                      onChange={() => {
+                        return;
+                      }}
+                      readOnly
+                      value={(reservation?.time?.from ?? 0) * 1000}
+                      renderInput={params => (
+                        <TextField {...params} size='small' disabled />
+                      )}
+                      ampm={false}
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TimePicker
+                      label='To'
+                      value={(reservation?.time?.to ?? 0) * 1000}
+                      readOnly
+                      onChange={() => {
+                        return;
+                      }}
+                      renderInput={params => (
+                        <TextField {...params} size='small' disabled />
+                      )}
+                      ampm={false}
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <DatePicker
+                      label='Date'
+                      value={(reservation?.time?.from ?? 0) * 1000}
+                      onChange={() => {
+                        return;
+                      }}
+                      renderInput={params => (
+                        <TextField
+                          {...params}
+                          size='small'
+                          fullWidth
+                          disabled
+                        />
+                      )}
+                      disabled
+                    />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} sm>
+                  <TextField
+                    label='Name'
+                    id='nameInput'
+                    defaultValue={reservation?.userName}
+                    type='text'
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    disabled
+                    fullWidth
+                    size='small'
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    label='Email'
+                    id='emailInput'
+                    defaultValue={reservation?.userEmail}
+                    type='email'
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    disabled
+                    fullWidth
+                    size='small'
+                  />
+                </Grid>
+                <Grid container item xs={12} spacing={2}>
+                  {!reservation.confirmed && confirmationToken && (
+                    <Grid item xs>
+                      <LoadingButton
+                        variant='contained'
+                        color='success'
+                        size='large'
+                        sx={{ boxShadow: 3, color: 'white' }}
+                        onClick={() => confirmReservation()}
+                        loading={isConfirmButtonLoading}
+                        disabled={isCancelButtonLoading}
+                        fullWidth
+                      >
+                        Confirm
+                      </LoadingButton>
+                    </Grid>
+                  )}
+                  {!isIn12Hours(reservation.time?.from ?? 0) && (
+                    <Grid item xs>
+                      <LoadingButton
+                        variant='contained'
+                        color='error'
+                        size='large'
+                        sx={{ boxShadow: 3, color: 'white' }}
+                        onClick={() => cancelReservation()}
+                        loading={isCancelButtonLoading}
+                        disabled={isConfirmButtonLoading}
+                        fullWidth
+                      >
+                        Cancel
+                      </LoadingButton>
+                    </Grid>
+                  )}
+                  <Grid item xs>
+                    <Button
+                      variant='contained'
+                      color='primary'
+                      size='large'
+                      href={`${configuration.basePath}/reservation/${reservation.id}/ics`}
+                      target='_blank'
+                      rel='noreferrer'
+                      sx={{ boxShadow: 5 }}
+                      download
+                      fullWidth
+                    >
+                      ICS File
+                    </Button>
+                  </Grid>
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid container item spacing={2}>
+                  <Grid item xs={3}>
+                    <Skeleton variant='rectangular' height={40} />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <Skeleton variant='rectangular' height={40} />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Skeleton variant='rectangular' height={40} />
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <Skeleton variant='rectangular' height={40} />
+                </Grid>
+                <Grid item xs={12}>
+                  <Skeleton variant='rectangular' height={40} />
+                </Grid>
+                <Grid item xs={12}>
+                  <Skeleton variant='rectangular' height={40} />
+                </Grid>
+              </>
+            ))}
+        </Grid>
+      </Grid>
       {restaurant && (
         <RestaurantDetailsModal
           open={detailModalOpen}
